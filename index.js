a071be788d7e4f487f579163b9e8b0e533042878
ljacobsson <lars@mathem.se>
2022-10-28 22:41:34 +0200

    adding best guess function

--- index.js
+++ index.js
@@ -18,7 +18,27 @@ function getSVG(cfnType, type = "svg", size = "64") {
     return;
   }
 }
+function guessSVG(service, type = "svg", size = "64") {
+  try {
+    const bestGuessKey = Object.keys(mapping).filter(p => p.toLowerCase().contains(service.toLowerCase()));
+    const bestGuess = mapping[bestGuessKey];
+    return fs
+      .readFileSync(
+        path.join(
+          __dirname,
+          bestGuess.icon
+            .replace(/\{size\}/g, size)
+            .replace("{type}", type)
+        )
+      )
+      .toString();
+  } catch (err) {
+    if (process.env.AWS_ICON_DIR_DEBUG === 1) console.log(err);
+    return;
+  }
+}
 
 module.exports = {
   getSVG,
+  guessSVG
 };
